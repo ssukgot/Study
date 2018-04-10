c7b7a1ef027000df41febae318e412ce182afb2d
ssukgot <ssukgot@gmail.com>
2018-04-10 17:25:12 +0900

    18.04.10 study_1

---
+++ Clicker/Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public Text goldDisplayer;
+
+    public Text goldPerClickDisplayer;
+
+    public DataController dataController;
+
+    void Update()
+    {
+        goldDisplayer.text = "GOLD : " + dataController.GetGold();
+        goldPerClickDisplayer.text = "GOLD Per Click : " + dataController.GetGoldPerClick();
+    }
+
+}
