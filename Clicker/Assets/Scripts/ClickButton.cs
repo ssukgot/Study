4d0ea5cad87209a9876aa01f0e6b1f2515228203
ssukgot <ssukgot@gmail.com>
2018-04-10 16:53:52 +0900

    18.04.10 study

---
+++ Clicker/Assets/Scripts/ClickButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButton : MonoBehaviour {
+
+    public DataController dataController;
+
+    public void OnClick()
+    {
+        //gold = gold + goldPerClick;
+        int goldPerClick = dataController.GetGoldPerClick();
+        dataController.AddGold(goldPerClick);
+
+    }
+
+}
